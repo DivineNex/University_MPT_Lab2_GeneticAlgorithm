11ceaa97a750b3905effc68ba081858b9bf0d437
Nikolay Makalov <nexterrible@mail.ru>
2022-11-11 01:00:12 +0700

    Добавьте файлы проекта.

---
+++ University_MPT_Lab2_GeneticAlgorithm/Form1.cs
@@ -0,0 +1,10 @@
+namespace University_MPT_Lab2_GeneticAlgorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
